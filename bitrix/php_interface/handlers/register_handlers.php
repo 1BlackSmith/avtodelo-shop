897293a3883348e9be99a376cb8960615f743d42
1BlackSmith <1BlackSmith.work@gmail.com>
2020-06-02 15:21:10 +0300

    fix register

--- bitrix/php_interface/handlers/register_handlers.php
+++ bitrix/php_interface/handlers/register_handlers.php
@@ -21,20 +21,20 @@ AddEventHandler(
 ); 
 
 class Register {
-    public function OnBeforeUserRegister(&$arFields)
-    {
-        $russianWord = "/^[А-Я]{1}[а-я]+$/";
+    // public function OnBeforeUserRegister(&$arFields)
+    // {
+    //     $russianWord = "/^[А-Я]{1}[а-я]+$/";
         
-        if (!preg_match($russianWord, $arFields['NAME'])) {
-            $GLOBALS['APPLICATION']->ThrowException('Имя должно быть введено строго на кириллице');
-            return false;
-        }
+    //     if (!preg_match($russianWord, $arFields['NAME'])) {
+    //         $GLOBALS['APPLICATION']->ThrowException('Имя должно быть введено строго на кириллице');
+    //         return false;
+    //     }
 
-        if (!empty($arFields['LAST_NAME'] && !preg_match($russianWord, $arFields['LAST_NAME']))) {
-            $GLOBALS['APPLICATION']->ThrowException('Фамилия должна быть введена строго на кириллице');
-            return false;
-        }
-    }
+    //     if (!empty($arFields['LAST_NAME'] && !preg_match($russianWord, $arFields['LAST_NAME']))) {
+    //         $GLOBALS['APPLICATION']->ThrowException('Фамилия должна быть введена строго на кириллице');
+    //         return false;
+    //     }
+    // }
 
     public function OnBeforeUserSimpleRegister(&$arFields)
     {
